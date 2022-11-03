532cd36a83e8025f1ff17346f7f8d68b74957f07
Hamza Moosa <hamzamoosa@Hamzas-MacBook-Air.local>
2022-11-03 15:41:07 +0000

    review filter is up and running

--- frontend/nc-games/src/components/reviewList.jsx
+++ frontend/nc-games/src/components/reviewList.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import { getReviews } from "../api";
-import { BrowserRouter, Routes, Route, Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
     const Reviewlist = ()=>{
     const [reviews, setReviews] = useState([]);
@@ -9,22 +9,22 @@ import { BrowserRouter, Routes, Route, Link, useParams } from "react-router-dom"
     //the useEffect is just data being loaded in/collected
     //always start a useEffect with something that shows the app is loading
         const {category} = useParams()
-        console.log(category)//gets current selected category from url
+        
     useEffect(()=>{ 
         getReviews()
         .then((data) => {
-            console.log(category)
-            if(category === undefined){   //each time the ==/=== alternated, the first console log returns an array (filtered) of objects containing the filtered reviews, then never again after reloads/genre changes.
-                console.log(data.reviews)
+            
+            if(category === undefined){   
+                
                 setReviews(data.reviews);
                 setLoading(false);
             }
-else{
+            else{
             const filtered = data.reviews.filter(review => 
             review.category === category  
             )
-            console.log(filtered)
-            setReviews(filtered); //when this stops working(after refresh) go back to menu then into categories and select a category, then change filtered to data.reviews and load all reviews, then change it back and the filter should work.
+            
+            setReviews(filtered); 
             setLoading(false);
 
         }
@@ -32,11 +32,10 @@ else{
     }, [category]);
 
 
-    if(loading) return <h2>loading...</h2>//loading message
+ if(loading) return <h2>loading...</h2>//loading message
 //set loading to false at the end of all useEffects, then add functionality in between setLoadings.
 return <ul>{reviews.map((review) => {
 return <li className="reviews" key={review['review_id']}> 
-<Link to={review.title}>{review.title}</Link>
 <p className="title">Title:</p>{review.title}
 <br></br>
 <p className="category">Category:</p>{review.category}
