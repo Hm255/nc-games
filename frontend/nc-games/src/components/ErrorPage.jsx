edb6dcbe5a00d00d8997f861921ae8e96231b88a
Hamza <Hamza@Arifs-MacBook-Pro.local>
2023-02-02 14:50:52 +0000

    more error handling work

--- frontend/nc-games/src/components/ErrorPage.jsx
+++ frontend/nc-games/src/components/ErrorPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ErrorPage = ({ errorMessage }) => {
-  return <div className="error">{errorMessage}</div>;
+  return <div className="error">{errorMessage} wrong place</div>;
 };
 
 export default ErrorPage;
